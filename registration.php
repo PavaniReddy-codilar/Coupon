c5e04ea4e67c8423198eaa2c380b083758977f19
PavaniReddy <94516590+PavaniReddy-codilar@users.noreply.github.com>
2022-01-31 19:20:02 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+/**
+ * Created By : Rohan Hapani
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Codilar_Coupon',
+    __DIR__ 
+);
